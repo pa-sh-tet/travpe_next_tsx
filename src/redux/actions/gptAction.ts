561048d09b1fa0456d64fa0a0a26a94ddeb88946
Pasha Altapov <2004pablo2004@gmail.com>
2026-04-20 21:38:39 +0300

    add api link in gpt

--- src/redux/actions/gptAction.ts
+++ src/redux/actions/gptAction.ts
@@ -1,11 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { api_url } from "@/utils/api";
+
+const API_URL = `${api_url}/gpt`;
 
 export const fetchGigaPlace = createAsyncThunk(
 	"gpt/fetchPlace",
 	async (_, thunkAPI) => {
 		try {
-			const response = await axios.get("http://localhost:5000/api/gpt/places");
+			const response = await axios.get(`${API_URL}/places`);
 			return response.data.place;
 		} catch (error) {
 			console.error(error);
